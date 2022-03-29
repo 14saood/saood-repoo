edec38802b148c85b4e21d5c98c451c49cbb86f4
14saood <56502172+14saood@users.noreply.github.com>
2022-03-29 14:04:37 +0530

    this is saood file

---
+++ saoodnew.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region     = "us-east-1"
+
+}
+resource "aws_instance" "terraform" {
+  ami= "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "saood_instance"
+  }
+}
+
+resource "aws_ebs_volume" "TFebs1" {
+ availability_zone = aws_instance.terraform.availability_zone
+ size = 1
+
+ tags = {
+  Name = "Extra_EBS"
+ }
+
+}
+resource "aws_volume_attachment" "attach_ebs_1" {
+device_name = "/dev/sdh"
+volume_id = aws_ebs_volume.TFebs1.id
+instance_id =aws_instance.terraform.id
+}
+resource "aws_eip" "lb" {
+  instance = aws_instance.terraform.id
+  vpc      = true
+}
